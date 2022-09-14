02aa1da1f55cb7e6aae79ba4304d53b5ab7839dc
ziansin <pilchza@ssfs.org>
2022-09-14 08:24:42 -0400

    house1

--- assignment_one.py
+++ assignment_one.py
@@ -1,1 +1,18 @@
 # remember to use comments!
+import turtle
+
+turtle.speed(100)
+
+for x in range(4):
+    turtle.left(90)
+    turtle.forward(100)
+turtle.left(90)
+turtle.penup()
+turtle.forward(100)
+turtle.left(-90)
+turtle.pendown()
+for x in range(2):
+    turtle.left(120)
+    turtle.forward(100)
+
+turtle.exitonclick()
