39c3728cbb834263e9025dc0b75817df3b3bf7c6
ziansin <pilchza@ssfs.org>
2022-09-14 08:59:00 -0400

    created a function to draw houses

---
+++ assignment_one.py
@@ -0,0 +1,2 @@
+
+# remember to use comments!
--- drawing_houses.py
+++ drawing_houses.py
@@ -1,18 +1,39 @@
 # remember to use comments!
 import turtle
-
+h = 0
 turtle.speed(100)
 
-for x in range(4):
+def makeASquare(squarecolor):
+    turtle.fillcolor(squarecolor)
+    for x in range(4):
+        turtle.left(90)
+        turtle.forward(100)
+
+def makeATriangle(color):
+    turtle.fillcolor(color)
+    for x in range(3):
+        turtle.left(120)
+        turtle.forward(100)
+
+def drawAHouse():
+    turtle.begin_fill()
+    makeASquare("red")
+    turtle.end_fill()
+    turtle.begin_fill()
     turtle.left(90)
+    turtle.penup()
     turtle.forward(100)
-turtle.left(90)
-turtle.penup()
-turtle.forward(100)
-turtle.left(-90)
-turtle.pendown()
-for x in range(2):
-    turtle.left(120)
-    turtle.forward(100)
+    turtle.left(-90)
+    turtle.pendown()
+    makeATriangle("blue")
+    turtle.end_fill()
+
+for x in range(4):
+    turtle.pendown()
+    drawAHouse()
+    turtle.penup()
+    h = h + 110
+    turtle.goto(h,0)
+
 
 turtle.exitonclick()
