3354ae5b2e10a7ce5cbfc8632b2cca656f3afb49
ziansin <pilchza@ssfs.org>
2022-09-16 13:52:40 -0400

    added sky and sun

--- drawing_houses.py
+++ drawing_houses.py
@@ -23,27 +23,43 @@ def drawAHouse(size, house_color, roof_color):
     turtle.end_fill()
 
 turtle.penup()
-turtle.goto(-200,0)
+turtle.goto(-400,-350)
+turtle.pendown()
+turtle.begin_fill()
+turtle.fillcolor("cyan")
+for x in range(4):
+    turtle.forward(1000)
+    turtle.left(90)
+turtle.end_fill()
+turtle.penup()
+turtle.goto(-200,-325)
 turtle.pendown()
 drawAHouse(100, "red", "blue")
 turtle.penup()
 h = h + 110
-turtle.goto(h,0)
+turtle.goto(h,-325)
 turtle.pendown()
 drawAHouse(120, "purple", "pink")
 turtle.penup()
 h = h + 110
-turtle.goto(h,0)
+turtle.goto(h,-325)
 turtle.pendown()
 drawAHouse(50, "green", "yellow")
 turtle.penup()
 h = h + 110
-turtle.goto(h,0)
+turtle.goto(h,-325)
 turtle.forward(50)
 turtle.pendown()
 drawAHouse(100, "brown", "black")
 turtle.penup()
-h = h + 110
-turtle.goto(h,0)
+turtle.goto(0,0)
+turtle.pendown()
+turtle.begin_fill()
+turtle.fillcolor("yellow")
+for x in range(120):
+    turtle.forward(3)
+    turtle.left(3)
+turtle.end_fill()
+
 
 turtle.exitonclick()
