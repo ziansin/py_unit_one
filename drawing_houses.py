b711db1fa79cc0005203538b986c21fdcebaa477
ziansin <pilchza@ssfs.org>
2022-09-16 13:40:29 -0400

    draw 4 house with different sizes and colors

--- drawing_houses.py
+++ drawing_houses.py
@@ -1,38 +1,49 @@
 # remember to use comments!
 import turtle
-h = 0
+h = -100
 turtle.speed(100)
 
-def makeASquare(squarecolor):
-    turtle.fillcolor(squarecolor)
+def drawAHouse(size, house_color, roof_color):
+    turtle.begin_fill()
+    turtle.fillcolor(house_color)
     for x in range(4):
         turtle.left(90)
-        turtle.forward(100)
-
-def makeATriangle(color):
-    turtle.fillcolor(color)
-    for x in range(3):
-        turtle.left(120)
-        turtle.forward(100)
-
-def drawAHouse():
-    turtle.begin_fill()
-    makeASquare("red")
+        turtle.forward(size)
     turtle.end_fill()
     turtle.begin_fill()
     turtle.left(90)
     turtle.penup()
-    turtle.forward(100)
+    turtle.forward(size)
     turtle.left(-90)
     turtle.pendown()
-    makeATriangle("blue")
+    turtle.fillcolor(roof_color)
+    for x in range(3):
+        turtle.left(120)
+        turtle.forward(size)
     turtle.end_fill()
 
-for x in range(4):
-    turtle.pendown()
-    drawAHouse()
-    turtle.penup()
-    h = h + 110
-    turtle.goto(h,0)
+turtle.penup()
+turtle.goto(-200,0)
+turtle.pendown()
+drawAHouse(100, "red", "blue")
+turtle.penup()
+h = h + 110
+turtle.goto(h,0)
+turtle.pendown()
+drawAHouse(120, "purple", "pink")
+turtle.penup()
+h = h + 110
+turtle.goto(h,0)
+turtle.pendown()
+drawAHouse(50, "green", "yellow")
+turtle.penup()
+h = h + 110
+turtle.goto(h,0)
+turtle.forward(50)
+turtle.pendown()
+drawAHouse(100, "brown", "black")
+turtle.penup()
+h = h + 110
+turtle.goto(h,0)
 
 turtle.exitonclick()
